979ff6d892586c2936922dca931321a60b3cbc9e
fgobron <fgobron@alternetdev.net>
2021-04-13 12:28:51 +0200

    feat(static-deployment): use webpack json-loader to fetch datas

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Application bootstrap module
  * @namespace Init
@@ -6,30 +7,29 @@
 import * as Dom from './utils/dom';
 import * as Data from './utils/data';
 import parseQueryString from './utils/query';
+import datas from '../datas.json';
 
 /* Application entrypoint */
 const init = () => {
   Dom.generateTitle();
-  // Fetch exhaustive clients list
-  Data.getList().then((datas) => {
-    let clients = Data.extractClients(datas);
-    // Browse parameters on the querystring
-    parseQueryString(window.location.search).forEach((item) => {
-      const key = item[0];
-      const entry = item[1];
-      switch (key) {
-        case 'eyeColor':
-          clients = Data.filterClientsBySimpleCriteria(clients, key, entry);
-          break;
-        case 'ageRange':
-          clients = Data.filterClientsByRangeCriteria(clients, 'age', entry);
-          break;
-        default:
-          // No filter
-      }
-    });
-    Dom.generateTable(clients);
+  // Extrac subSet clients list from exhaustive datas
+  let clients = Data.extractClients(datas);
+  // Browse parameters on the querystring
+  parseQueryString(window.location.search).forEach((item) => {
+    const key = item[0];
+    const entry = item[1];
+    switch (key) {
+      case 'eyeColor':
+        clients = Data.filterClientsBySimpleCriteria(clients, key, entry);
+        break;
+      case 'ageRange':
+        clients = Data.filterClientsByRangeCriteria(clients, 'age', entry);
+        break;
+      default:
+        // No filter
+    }
   });
+  Dom.generateTable(clients);
 };
 
 init();
